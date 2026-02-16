9a1d5c9fa1166509ae27db7ea498e6db708d3dfc
Kareem Ragab <kragab538@gmail.com>
2026-02-16 22:42:25 +0200

    deploy baseURL

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 // import i18n from "i18next";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
-  //  baseURL: "https://rydroo-app-backend.onrender.com/",
+  // baseURL: "http://localhost:5000/",
+   baseURL: "https://rydroo-app-backend.onrender.com/",
   withCredentials : true,
   //  headers: {
   //   lang: i18n.language
